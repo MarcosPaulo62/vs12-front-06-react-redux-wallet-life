93ea3c462dd29839827ef9ca06993d5b37c8125a
Annelise Silva da Rocha <annelisesr@gmail.com>
2023-08-23 01:44:28 -0300

    wip contato lp anne

---
+++ src/components/contatoLp/ContatoLp.tsx
@@ -0,0 +1,94 @@
+import { StyledTitle } from "../../styles/typography";
+import { ContainerContatoLp, FormContatoLP } from "./style";
+import { useForm } from "react-hook-form";
+
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+interface FormData {
+  nome: string;
+  email: string;
+  duvida: string;
+}
+
+export default function ContatoLp() {
+  const { register, handleSubmit, reset } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+
+    const validateEmail = (email: string) => {
+      const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      return regex.test(email);
+    };
+
+    if (!validateEmail(data.email)) {
+      toast.warning(
+        "É necessário que seu e-mail esteja completo! Exemplo: seuemail@email.com",
+        {
+          position: toast.POSITION.TOP_RIGHT,
+        }
+      );
+    } else if (!data.nome.trim() || !data.email.trim() || !data.duvida.trim()) {
+      toast.warning("É necessário preencher todos os campos!", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } else {
+      toast.success("Sua dúvida foi enviada!", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      reset();
+    }
+  };
+
+  return (
+    <ContainerContatoLp>
+      <StyledTitle
+        className="styled-title"
+        fontWeight={700}
+        tag={"h2"}
+        fontSize={"lg"}
+      >
+        Ficou com alguma dúvida?
+      </StyledTitle>
+
+      <FormContatoLP onSubmit={handleSubmit(onSubmit)}>
+        <StyledTitle
+          className="styled-title-fale-conosco"
+          fontWeight={400}
+          fontSize={"lg"}
+          tag={"h3"}
+        >
+          Fale conosco!
+        </StyledTitle>
+        <div>
+          <input
+            className="input"
+            placeholder="seu nome"
+            {...register("nome")}
+          />
+        </div>
+        <div>
+          <input
+            className="input"
+            type="email"
+            placeholder="seu e-mail"
+            {...register("email")}
+          />
+        </div>
+        <div>
+          <textarea
+            className="textarea"
+            placeholder="sua dúvida"
+            {...register("duvida")}
+          />
+        </div>
+        <div>
+          <button type="submit">Enviar</button>
+        </div>
+      </FormContatoLP>
+      <ToastContainer />
+    </ContainerContatoLp>
+  );
+}
---
+++ src/components/contatoLp/style.ts
@@ -0,0 +1,58 @@
+import { styled } from "styled-components"
+
+export const ContainerContatoLp = styled.div `
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+background-color: ${({theme})=>theme.color.jet};
+width: 90%;
+border-radius: 32px;
+margin: auto;
+padding: 45px 85px;
+box-sizing: border-box;
+height: 555px;
+
+.styled-title{
+    width: 35%;
+    color: ${({theme})=>theme.color.white};
+}
+
+.styled-title-fale-conosco{
+    font-weight: bold;
+    font-size: 44px;
+}
+`
+
+export const FormContatoLP = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+width: 55%;
+height: 100%;
+background-color: ${({theme})=>theme.color.white};
+padding-block: 40px;
+border-radius: 32px;
+margin-right: -30px;
+gap: 20px;
+
+
+.input{
+    width: 415px;
+    padding: 15px;
+    height: 60px;
+    border-radius: 12px;
+    border: none;
+    background-color: ${({theme})=>theme.color.alabaster};
+}
+
+.textarea{
+    width: 415px;
+    padding: 15px;
+    height: 120px;
+    border-radius: 12px;
+    border: none;
+    background-color: ${({theme})=>theme.color.alabaster};
+}
+`
