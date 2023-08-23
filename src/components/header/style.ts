95a08111a1bf912e5798cd876f6675dd8cd5475d
Glauber Bispo <glauber.bispo@gmail.com>
2023-08-22 23:29:25 -0300

    added default button styles

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -19,24 +19,10 @@ export const StyledHeader = styled.header`
         display: flex;
         align-items: center;
         margin-right:6.62rem;
-        gap: 1.81rem;
+        gap: 1.81rem;   
 
         .btn {
-            border-radius: 0.75rem;
             height: 4.375rem;
-            background-color: ${({theme}) => theme.color.brownSugar};
-            color: ${({theme}) => theme.color.white};
-            font-size: 1.75rem;
-            font-weight: 700;
-            margin: 1.75rem 0;
-        }
-
-        .btn-cadastro {
-            width:13.81rem;
-        }
-
-        .btn-login {
-            width: 10.3rem;
         }
 
     }
