859b4596b68b287401a8ce5c2cf95e18d0fefa8b
Annelise Silva da Rocha <annelisesr@gmail.com>
2023-08-24 01:24:18 -0300

    token de validação de rotas privadas e definição das mesmas

---
+++ src/pages/despesas/Despesas.tsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router-dom";
+
+export default function Despesas() {
+    return(
+        <div>
+            <h1>DESPESAAAAAAAAAAAAAS</h1>
+            <NavLink to="/login"><button>SAIR</button></NavLink>
+        </div>
+    )
+}
