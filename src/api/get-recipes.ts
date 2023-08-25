59d067cc98619b12813265a7c0a6f5895b476098
therosenstock <fabiolars2009@hotmail.com>
2023-08-25 00:14:10 -0300

    receitas

--- src/api/get-recipes.ts
+++ src/api/get-recipes.ts
@@ -1,5 +1,19 @@
+import { getUserLogged } from './get-user-logged';
 import {http} from './http';
 
-export async function getRecipes(){
+export async function getRecipes(pagina : number, quantidadeRegistros: number){
+  const token = localStorage.getItem('token');
 
+  const response = await http.get<GetRecipesData>(`/usuario/usuario-receitas`,{ headers:{ Authorization: token}, params: { pagina: pagina,
+    quantidadeRegistros: quantidadeRegistros}});
+    return response.data;
+}
+
+export type GetRecipesData = {
+  idUsuario: number,
+  nome: string,
+  idReceita: number,
+  valor: number,
+  descricao: string,
+  banco: string
 }
