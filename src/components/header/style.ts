ea44421ef0a453d3f3d54e0134e0b601edf09296
Rafael <rafaelgesteque@gmail.com>
2023-08-25 03:48:52 -0300

    responsividade landing page ok

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,31 +1,62 @@
 import { styled } from "styled-components";
 
 export const StyledHeader = styled.header`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  height: 14.6rem;
+  width: 100vw;
+  background-color: ${({ theme }) => theme.color.alabaster};
 
+  img {
+    width: 20.43rem;
+    height: 11.06rem;
+    margin-left: 4.3rem;
+  }
+
+  .btns-div {
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-between;
+  }
+
+  div {
+    display: flex;
     align-items: center;
-    height: 14.6rem;
-    width: 100vw;
-    background-color: ${({theme}) => theme.color.alabaster};
+    margin-right: 6.62rem;
+    gap: 1.81rem;
 
-    img{
-        width: 20.43rem;
-        height: 11.06rem;
-        margin-left:4.3rem;        
+    .btn {
+      height: 4.375rem;
     }
+  }
 
-    div {
+  @media screen and (max-width: 1000px) {
+    height: 20rem;
+
+    img {
+      max-width: 90vw;
+      height: 11.06rem;
+      margin-left: 4.3rem;
+    }
+  }
+
+  @media screen and (max-width: 1000px) {
+    height: 25rem;
+    justify-content: center;
+    align-items: center;
+
+    .btns-div {
+        margin: auto;
         display: flex;
-        align-items: center;
-        margin-right:6.62rem;
-        gap: 1.81rem;   
-
-        .btn {
-            height: 4.375rem;
-        }
-
+        flex-wrap: wrap;
+        justify-content: center;
     }
 
-`
+    img {
+      max-width: 100%;
+      height: 11.06rem;
+      margin: 0;
+    }
+  }
+`;
